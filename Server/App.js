848707f8af2dde79cd853d609a6df50c85af0a49
unknown <kunal.siwal@gmail.com>
2022-09-17 00:53:12 +0530

    solved api problem

--- Server/App.js
+++ Server/App.js
@@ -18,28 +18,38 @@ app.post('/destination',(req,res)=>{
     try{
 
     
-    const options = {
-        method: 'POST',
-        url: 'https://travel-places.p.rapidapi.com/',
+      const options = {
+        method: 'GET',
+        url: 'https://webcamstravel.p.rapidapi.com/webcams/list/nearby=%7Blat%7D,%7Blng%7D,%7Bradius%7D',
+        params: {lang: 'en', show: 'webcams:image,location'},
         headers: {
           'X-RapidAPI-Key': '0647c6a7fdmsh1145dfe8ff827c3p198cd9jsn999ff72dded7',
-          'X-RapidAPI-Host': 'travel-places.p.rapidapi.com'
+          'X-RapidAPI-Host': 'webcamstravel.p.rapidapi.com'
         }
-    };
+      };
 
-    let data;
+    //destina/dehradun
     async function getData(){
         
-        data = await axios.request(options)
-        console.log(data);
+        let data = await axios.request(options)
+        return data;
+        
         
         
     }
-    getData();
-   console.log("destination");
-   res.send("tooo");
+    let data =getData();
+    data.then((result)=>{console.log(result.data.result.webcams[0]);
+      console.log("inside then")
+    res.send(result.data.result.webcams[0])}).catch((err)=>{
+      console.log(err);
+      console.log("inside errro")
+      res.send(err)
+    })
+  
+   
    }
    catch(error) {
+    console.log("here in error")
     console.error(error);
    }
 })
